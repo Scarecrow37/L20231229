a425dbb1fb1f24f09a02a57f97744255bb53aee4
Suha Do <weqa123@naver.com>
2023-12-29 09:06:57 +0900

    Create ThirdPersonAnimBlueprint.cpp

---
+++ Source/L20231229/Public/Animations/ThirdPersonAnimBlueprint.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "ThirdPersonAnimBlueprint.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class L20231229_API UThirdPersonAnimBlueprint : public UAnimInstance
+{
+	GENERATED_BODY()
+	
+};
