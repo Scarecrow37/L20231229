9546d9affb0ef355303591141ae99c31a99aea4c
Suha Do <weqa123@naver.com>
2023-12-29 10:22:07 +0900

    Create variable for character movement animation.

--- Source/L20231229/Private/Animations/ThirdPersonAnimBlueprint.cpp
+++ Source/L20231229/Private/Animations/ThirdPersonAnimBlueprint.cpp
@@ -3,3 +3,30 @@
 
 #include "Animations/ThirdPersonAnimBlueprint.h"
 
+#include "GameFramework/Character.h"
+#include "GameFramework/PawnMovementComponent.h"
+
+UThirdPersonAnimBlueprint::UThirdPersonAnimBlueprint(): GroundSpeed(0.0f), IsFalling(false), IsJumping(false),
+                                                        ShouldMove(false), ShowingAnimationJumpSpeed(100.0f), HidingAnimationMovementSpeed(3.0f)
+{
+}
+
+void UThirdPersonAnimBlueprint::NativeUpdateAnimation(float DeltaSeconds)
+{
+	Super::NativeUpdateAnimation(DeltaSeconds);
+
+	if (const APawn* Pawn = TryGetPawnOwner(); IsValid(Pawn))
+	{
+		Velocity = Pawn->GetVelocity();
+		Acceleration = Velocity - PreviousVelocity;
+		PreviousVelocity = Velocity;
+		GroundSpeed = Velocity.Size2D();
+		if (const ACharacter* Character = Cast<ACharacter>(Pawn); IsValid(Character))
+		{
+			const UPawnMovementComponent* Movement = Character->GetMovementComponent();
+			IsFalling = Movement->IsFalling();
+			IsJumping = IsFalling && Velocity.Z > ShowingAnimationJumpSpeed;
+			ShouldMove = Acceleration != FVector::Zero() && GroundSpeed > HidingAnimationMovementSpeed; 
+		}
+	}
+}
--- Source/L20231229/Public/Animations/ThirdPersonAnimBlueprint.h
+++ Source/L20231229/Public/Animations/ThirdPersonAnimBlueprint.h
@@ -13,5 +13,34 @@ UCLASS()
 class L20231229_API UThirdPersonAnimBlueprint : public UAnimInstance
 {
 	GENERATED_BODY()
+
+public:
+	UThirdPersonAnimBlueprint();
+
+public:
+	virtual void NativeUpdateAnimation(float DeltaSeconds) override;
+
+private:
+	FVector Velocity;
+	FVector PreviousVelocity;
+	FVector Acceleration;
 	
+private:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Movement", meta = (AllowPrivateAccess = "true"))
+	float GroundSpeed;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Movement", meta = (AllowPrivateAccess = "true"))
+	bool IsFalling;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Movement", meta = (AllowPrivateAccess = "true"))
+	bool IsJumping;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Movement", meta = (AllowPrivateAccess = "true"))
+	bool ShouldMove;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Movement", meta = (AllowPrivateAccess = "true"))
+	float ShowingAnimationJumpSpeed;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Movement", meta = (AllowPrivateAccess = "true"))
+	float HidingAnimationMovementSpeed;
 };
